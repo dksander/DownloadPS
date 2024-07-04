659fe47474332aef5cd48cfe40e7c9c9f1430d80
SanderDK <lars.sander@live.dk>
2024-07-04 10:52:19 +0200

    Update DeployOnPrem2.ps1

--- DeployOnPrem2.ps1
+++ DeployOnPrem2.ps1
@@ -1,1 +1,76 @@
-Write-host "Hello World"
+<# 
+ .Synopsis
+ Script to run deploy on from AL-Go-PTE
+ .Description
+    To Be Developed
+#>
+
+param(
+    $BCVersion,
+    $BCInstance,
+    $syncMode,
+    $appPath,
+    $appName,
+)
+function PreLoadModule() {
+    if (!(Get-Package -Name BcContainerHelper -ErrorAction Ignore)) {
+        Write-Host "Installing BCContainerHelper PowerShell package"
+        Install-Package BcContainerHelper -Force -WarningAction Ignore | Out-Null
+    }
+}
+
+function ImportModule{
+    Param (
+        [string] $modulePath
+    )
+    
+    if (Test-Path $modulePath) {
+        Import-Module $modulePath -WarningAction SilentlyContinue | Out-Null 
+    } else {
+        throw "Could not find $modulePath"
+    }
+}
+Function TestBCServiceIsRunning() {
+  $navServerServiceName =  "MicrosoftDynamicsNavServer`$" + $BCInstance;
+  try {
+      $nstStatus = (get-service "$navServerServiceName").Status;
+      if ($nstStatus -ne 'Running') {
+          throw "$navServerServiceName service not running"
+      }
+  } catch {
+      throw "$navServerServiceName service not installed"
+  }
+}
+
+#Do some pretest on App files
+###############################
+
+#Testing service
+TestBCServiceIsRunning();
+PreLoadModule;
+
+
+######
+#Stop service
+#####
+
+
+#####
+# Sort app
+#####
+#####
+# test if app needs to be published
+#####
+#####
+# publish app / upgrade app
+#####
+#####
+# Start services again
+#####
+#####
+# Write back new apps installed
+#####
+
+
+
+
